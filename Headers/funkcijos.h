15d0a0c125e869f799b9109250fcc2c850233097
Tomas Petronis <tomaspetronis77gmail.com>
2024-10-17 00:48:58 +0300

    Pradetas daryti  Genesis bloko kasimas

--- Headers/funkcijos.h
+++ Headers/funkcijos.h
@@ -11,13 +11,19 @@
 #include <random>
 #include <fstream>
 
+class User;
+class Transaction;
+class Block;
+class Data;
+
 void PaddedInput(std::string& simboliu_seka);
 void salt(std::string& simboliu_seka);
 std::string Maisos_funkcija(const std::string simboliu_seka);
-void GenerateUsers(int kiekis);
-void GenerateTransactions(int kiekis);
+void GenerateUsers(int kiekis, std::vector<User>& users);
+void GenerateTransactions(int kiekis, std::vector<Transaction>& transactions);
 std::string RandomStringGeneravimas(int ilgis);
-double RandomBalansoGeneravimas();
+double RandomSkaicius(double low, double high);
+std::vector<std::vector<Transaction>> GenerateCandidates(std::vector<Transaction>& transactions, int BlockSize);
 
 extern std::string simboliu_seka;
 
--- Sources/generavimas.cpp
+++ Sources/generavimas.cpp
@@ -3,14 +3,13 @@
 #include "../Headers/User.h"
 #include "../Headers/Block.h"
 
-void GenerateUsers(int kiekis) {
+void GenerateUsers(int kiekis, std::vector<User>& users){
     std::ofstream RF("Vartotojai.txt");
-    std::vector<User> users(kiekis);
 
     for(int i = 0; i < kiekis; i++){
         users[i].SetName("Vardas" + std::to_string(i+1));
         users[i].SetPublicKey(Maisos_funkcija(RandomStringGeneravimas(64)));
-        users[i].SetBalansas(std::round(RandomBalansoGeneravimas()*100.00)/100.00);
+        users[i].SetBalansas(std::round(RandomSkaicius(100.00, 1000000.00)*100.00)/100.00);
     }
 
     for(int i = 0; i < kiekis; i++){
@@ -44,26 +43,25 @@ std::string RandomStringGeneravimas(int ilgis) {
     return random_string;
 }
 
-double RandomBalansoGeneravimas(){
+double RandomSkaicius(double low, double high){
     std::random_device rd;
     std::mt19937 generator(rd());
-    std::uniform_real_distribution<double> distribution(100.0, 1000000.0);
+    std::uniform_real_distribution<double> distribution(low, high);
     return distribution(generator);
 }
 
-void GenerateTransactions(int kiekis){
+void GenerateTransactions(int kiekis, std::vector<Transaction>& transactions){
     std::ofstream RF("Transakcijos.txt");
-    std::vector<Transaction> transactions(kiekis);
 
     for(int i = 0; i < kiekis; i++){ 
         transactions[i].SetSender(RandomStringGeneravimas(64));
         transactions[i].SetReceiver(RandomStringGeneravimas(64));
-        transactions[i].SetAmount(RandomBalansoGeneravimas());
+        transactions[i].SetAmount(RandomSkaicius(100.00,1000000.00));
         transactions[i].SetTransactionID();
-        //transactions[i] = Transaction(RandomStringGeneravimas(64), RandomStringGeneravimas(64), RandomBalansoGeneravimas());
+        //transactions[i] = Transaction(RandomStringGeneravimas(64), RandomStringGeneravimas(64), RandomSkaicius());
     }
 
-    for(int i = 0; i < kiekis; i++){
+    /*for(int i = 0; i < kiekis; i++){
         for(int j = 0; j < kiekis; j++){
             if(transactions[i].GetTransactionID() == transactions[j].GetTransactionID() && i != j){
                 std::cout << "RASTA SUTAMPANCIU TRANSACTIONID... TRANSAKCIJOS NEBEGENERUOJAMOS" << std::endl;
@@ -72,7 +70,7 @@ void GenerateTransactions(int kiekis){
         }
     }
 
-    std::cout << "Viskas good! Nera sutampanciu TransactionID" << std::endl;
+    std::cout << "Viskas good! Nera sutampanciu TransactionID" << std::endl;*/
 
     RF << std::left << std::setw(70) << "Transakcijos ID:" << std::setw(70) << "Siuntejas:" << std::setw(70) << "Gavejas:" << std::setw(15) << "Suma:" << std::endl;
     for(int i = 0; i < kiekis; i++){
@@ -81,19 +79,38 @@ void GenerateTransactions(int kiekis){
     }
 }
 
-/*void uzduotis3(int kiekis){
-    std::ifstream DF("Transakcijos.txt");
-    std::ofstream RF("Blokai.txt");
-    std::vector<Transaction> transactions(kiekis);
-    std::vector<Block> blokas;
-    std::string;
+std::vector<std::vector<Transaction>> GenerateCandidates(std::vector<Transaction>& transactions, int BlockSize){
+    int kandidatu_kiekis = 5;
+    int transakciju_kiekis = (transactions.size() <= BlockSize) ? transactions.size() : BlockSize;
+
+    std::vector<std::vector<Transaction>> Kandidatu_sarasas;
+    std::vector<Transaction> Kandidatu_saraso_kopija = transactions;
+
+    for(int i = 0; i < kandidatu_kiekis; i++){
+        std::vector<Transaction> Kandidatai;
+
+        for(int j = 0; j < transakciju_kiekis; j++){
+            int kandidato_indeksas = RandomSkaicius(0, Kandidatu_saraso_kopija.size()-1);
+            Kandidatai.push_back(Kandidatu_saraso_kopija[kandidato_indeksas]);
+            Kandidatu_saraso_kopija.erase(Kandidatu_saraso_kopija.begin() + kandidato_indeksas);
+        }
+        
+        Kandidatu_sarasas.push_back(Kandidatai);
+    }
+    return Kandidatu_sarasas;
+}
+
+/*void uzduotis3(int kiekis, std::vector<Block>& blokai, std::vector<Transaction>& transactions){
+    for(int i = 0; i < kiekis; i++){
+        blokai[].
+    }
     
 }*/
 
 /*Block GenerateGenesisBlock(){
     std::vector<Transaction> transactions;
     std::vector<Block> blokai;
-    transactions.push_back(Transaction("Genesis", "Genesis", RandomBalansoGeneravimas()));
+    transactions.push_back(Transaction("Genesis", "Genesis", RandomSkaicius()));
     Block genesis("0", std::time(0), "1.0", 0, transactions);
 
 }*/
