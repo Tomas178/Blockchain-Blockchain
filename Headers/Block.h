47daf11d857305535e561bc8ed9e5a60d39e37b6
Tomas Petronis <tomaspetronis77gmail.com>
2024-10-16 11:46:29 +0300

    Patobulintas isvedimas ir pridetas patikrinimas

--- Headers/Block.h
+++ Headers/Block.h
@@ -28,6 +28,26 @@ class Block {
             this->Transactions = Transactions;
         }
 
+        void SetHash(std::string Hash) {this->Hash = Hash;};
+        void SetPreviousHash(std::string PreviousHash) {this->PreviousHash = PreviousHash;};
+        void SetTimestamp(std::time_t Timestamp) {this->Timestamp = Timestamp;};
+        void SetVersion(std::string Version) {this->Version = Version;};
+        void SetMerkleHash(std::string MerkleHash) {this->MerkleHash = MerkleHash;};
+        void SetNonce(int Nonce) {this->Nonce = Nonce;};
+        void SetDifficulty(int Difficulty) {this->Difficulty = Difficulty;};
+        void SetMined(bool Mined) {this->Mined = Mined;};
+        void SetTransactions(std::vector<Transaction> Transactions) {this->Transactions = Transactions;};
+
+        std::string GetHash() const {return Hash;};
+        std::string GetPreviousHash() const {return PreviousHash;};
+        std::time_t GetTimestamp() const {return Timestamp;};
+        std::string GetVersion() const {return Version;};
+        std::string GetMerkleHash() const {return MerkleHash;};
+        int GetNonce() const {return Nonce;};
+        int GetDifficulty() const {return Difficulty;};
+        bool GetMined() const {return Mined;};
+        std::vector<Transaction> GetTransactions() const {return Transactions;};
+
 };
 
 #endif
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -3,11 +3,10 @@
 int main(){
     std::cout << "Vyksta 1 uzduotis... Generuojama 1000 tinklo vartotoju" << std::endl;
     GenerateUsers(1000);
-    std::cout << "Vartotoju duomenys galima rasti Vartotojai.txt" << std::endl;
-    std::cout << std::endl;
+    std::cout << "Vartotoju duomenys galima rasti Vartotojai.txt" << std::endl << std::endl;
     
     std::cout << "Vyksta 2 uzduotis... Generuojama 10000 tinklo transakciju" << std::endl;
     GenerateTransactions(10000);
-    std::cout << "Transakciju duomenys galima rasti Transakcijos.txt" << std::endl;
+    std::cout << "Transakciju duomenys galima rasti Transakcijos.txt" << std::endl << std::endl;
     return 0;
 }
